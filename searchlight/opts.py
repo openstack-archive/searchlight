697ea9b81e1bd3b3da911beb3a511f9a69130b0b
Louis Taylor <louis@kragniz.eu>
2015-05-31 00:04:24 +0100

    Add options from searchlight.common.wsgi

--- searchlight/opts.py
+++ searchlight/opts.py
@@ -1,10 +1,14 @@
 import itertools
 
-import searchlight.service
+import searchlight.common.wsgi
 
 
 def list_opts():
     return [
         ('DEFAULT',
-         itertools.chain(searchlight.service.OPTS))
+         itertools.chain(searchlight.common.wsgi.bind_opts,
+                         searchlight.common.wsgi.socket_opts,
+                         searchlight.common.wsgi.eventlet_opts)),
+        ('profiler',
+         itertools.chain(searchlight.common.wsgi.profiler_opts)),
     ]
