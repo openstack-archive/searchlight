43e9a0c6be27ba85112a76e4ebf7414f317f2dfd
leizhang <lei.a.zhang@intel.com>
2017-04-13 12:25:43 +0800

    Deploy Searchlight api using uwsgi
    Add a WSGI app script and uwsgi configuration templates.
    In this patch modify devstack scripts to deploy Searchight
    api using uwsgi because mod_wsgi way is considered deprecated.
    
    Implements: blueprint deploy-via-wsgi
    Change-Id: I29c81bbf828778e4e9418c9b0f3d14b67cf02c17

---
+++ searchlight/api/wsgi.py
@@ -0,0 +1,22 @@
+# Copyright 2017 Intel Corporation
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from searchlight.cmd.api import configure_wsgi
+from searchlight.common import config
+
+
+def init_application():
+    configure_wsgi()
+    return config.load_paste_app('searchlight')
